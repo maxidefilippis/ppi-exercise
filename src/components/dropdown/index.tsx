6b2f41ed9d37d466160824bcaa6b651cff887733
maxidefilippis <mdefilippis@encamina.com>
2024-03-17 17:09:52 -0300

    created form components

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './index.scss';
+
+interface DropdownProps {
+    id: string;
+    label: string;
+    options: string[];
+    value: string | number;
+    onChange: () => void;
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({ id, label, options, value, onChange }) => {
+    return (
+        <div className="dropdown">
+            <label htmlFor={id}>{label}</label>
+            <select
+                value={value}
+                onChange={onChange}
+            >
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
---
+++ src/components/input/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './index.scss';
+
+interface InputProps {
+    id: string;
+    label: string;
+    type: string;
+    value: string | number;
+    onChange: () => void;
+    placeholder: string;
+}
+
+export const Input: React.FC<InputProps> = ({ id, label, type, value, onChange, placeholder }) => {
+    return (
+        <div className="input">
+            <label htmlFor={id}>{label}</label>
+            <input
+                id={id}
+                name={id}
+                type={type}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+            />
+        </div>
+    );
+};
