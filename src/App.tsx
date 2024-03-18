855a6d226eb09fd79a46e8f3385123bcbecc1a87
maxidefilippis <mdefilippis@encamina.com>
2024-03-17 21:23:43 -0300

    added media queries for desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import convert from './assets/convert.svg';
 import { Dropdown } from './components/dropdown';
 import { Input } from './components/input';
+import { LastUpdated } from './components/lastUpdated';
 
 import './App.scss';
 
@@ -15,41 +16,50 @@ function App() {
                     <h1>100 EUR to USD - Convert Euros to US Dollars</h1>
                 </div>
                 <div className="convertionCard">
-                    <Input
-                        id="amount"
-                        label="Amount"
-                        onChange={() => {}}
-                        placeholder=""
-                        type="number"
-                        value={100}
-                    />
-                    <Dropdown
-                        id="from"
-                        label="From"
-                        onChange={() => {}}
-                        value={'Euro'}
-                        options={['Euro', 'Dollar']}
-                    />
-                    <button className="swap">
-                        <img src={convert} />
-                    </button>
-                    <Dropdown
-                        id="to"
-                        label="To"
-                        onChange={() => {}}
-                        value={'Dollar'}
-                        options={['Euro', 'Dollar']}
-                    />
-                    <div className="convertionInfo">
-                        <p className="convertionByFrom">1.00 Euro = 1.0627478 US Dollars</p>
-                        <p className="convertionByTo">1 USD = 0.941004 EUR</p>
+                    <div className="convertionCard__form">
+                        <Input
+                            id="amount"
+                            label="Amount"
+                            onChange={() => {}}
+                            placeholder=""
+                            type="number"
+                            value={100}
+                        />
+                        <Dropdown
+                            id="from"
+                            label="From"
+                            onChange={() => {}}
+                            value={'Euro'}
+                            options={['Euro', 'Dollar']}
+                        />
+                        <button className="convertionCard__swap">
+                            <img src={convert} />
+                        </button>
+                        <Dropdown
+                            id="to"
+                            label="To"
+                            onChange={() => {}}
+                            value={'Dollar'}
+                            options={['Euro', 'Dollar']}
+                        />
+                    </div>
+                    <div className="convertion">
+                        <div className="convertion__information">
+                            <p className="convertion__byFrom">1.00 Euro = 1.0627478 US Dollars</p>
+                            <p className="convertion__byTo">1 USD = 0.941004 EUR</p>
+                        </div>
+                        <div className="convertion__lastUpdated">
+                            <p className="convertion__caution">
+                                We use the mid-market rate for our Converter. This is for informational purposes only. You won’t receive this rate
+                                when sending money.
+                            </p>
+                            <LastUpdated />
+                        </div>
                     </div>
                 </div>
-                <div className="lastUpdatedInformation">
-                    <p>
-                        <u>Euro</u> to <u>US Dollar</u> conversion — Last updated Dec 15, 2022,  19:17 UTC
-                    </p>
-                </div>
+                <footer>
+                    <LastUpdated />
+                </footer>
             </main>
         </>
     );
---
+++ src/components/lastUpdated/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+export const LastUpdated: React.FC = () => {
+    return (
+        <div className="lastUpdated">
+            <p>
+                <u>Euro</u> to <u>US Dollar</u> conversion — Last updated Dec 15, 2022, 19:17 UTC
+            </p>
+        </div>
+    );
+};
